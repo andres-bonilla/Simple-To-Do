f8f8ac14608c4b0cc1ad5b162bc1df0de122d3be
andres-bonilla <x.andresbonilla@gmail.com>
2023-08-23 01:29:04 -0300

    Se agrego localStorage y mejoras visuales

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-   let todo = {
+  import { browser } from '$app/environment';
+
+  let todo = {
     id: "",
     text: "",
     state: false
@@ -8,7 +10,7 @@
   const addTask = () => {
     if ( todo.text.trim() ){
       todo.id = Date.now()
-      todoList = [ ...todoList, todo ]
+      todoList = [ todo, ...todoList ]
       todo = {
         id: "",
         text: "",
@@ -32,6 +34,12 @@
     )
   }
 
+  if (browser && localStorage.getItem("todos") )
+    todoList = JSON.parse( localStorage.getItem( "todos" ) ) || todoList
+  
+  $: browser 
+    ? localStorage.setItem("todos", JSON.stringify( todoList ) ) 
+    : console.log("Browser ERROR")
 </script>
 
 <main> 
@@ -46,42 +54,66 @@
   </form>
 
   {#each todoList as item}
-    <div class={item.state ? "task-on" : "task-off" }>
+    <div class= {item.state ? "task-on taskBox" : "task-off taskBox"}>
       <p>
         {item.text}
       </p>
-      <button class="inline" on:click={() => { checkTask( item.id ) }}>
-        { #if item.state }
+      <div class="buttons">
+        <button class="inlineButton" on:click={() => { checkTask( item.id ) }}>
+          { #if item.state }
           <img src="src/essets/on.svg" alt="onIcon"/>
-        {:else}
+          {:else}
           <img src="src/essets/off.svg" alt="offIcon"/>
-        {/if}
-      </button>
-      <button class="inline" on:click={()=>{ removeTask( item.id )}}>
-        <img src="src/essets/trash.svg" alt="trashIcon"/>
-      </button>
+          {/if}
+        </button>
+        <button class="inlineButton" on:click={()=>{ removeTask( item.id )}}>
+          <img src="src/essets/trash.svg" alt="trashIcon"/>
+        </button>
+      </div>
     </div>
   {/each}
 </main>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap');
+  main {
+    min-width: 340px;
+    font-family: 'Maven Pro', sans-serif;
+    text-align: center;
+  }
   h1 {
-    color: rgb(255, 62, 0);
-    text-transform: uppercase;
-    font-size: 4em;
+    margin: 32px auto;
+    font-size: 44px;
     font-weight: 100;
-    margin: 20px auto;
+    color: rgb(255, 62, 0);
+  }
+  input {
+    width: 50%;
+    min-width: 270px;
+    padding: 10px 12px;
+    border-radius:12px;
+    margin-bottom: 24px;
+    font-size: 20px;
   }
   p {
+    width: 70%;
+    margin: 12px 0px 12px 12px;
     font-size: 22px;
-    text-align: left;
     font-weight: 400;
-    width: 360px;
+    text-align: left;
+  }
+  .taskBox {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 65%;
+    min-width: 340px;
+    border-radius:12px;
   }
   .task-off {
     margin: 7px auto;
-    background: rgb(255, 238, 228);
     border: 2px solid rgb(255, 62, 0);
+    background: rgb(255, 238, 228);
     p {
       color: rgb(255, 62, 0);
     }
@@ -89,15 +121,21 @@
 
   .task-on {
     margin: 8px auto;
-    background: rgb(255, 248, 244);
     border: 1px solid rgb(255, 62, 0);
+    background: rgb(255, 248, 244);
     p {
       color: rgb(255, 191, 169)
     }
   }
-
-  .inline {
+  .inlineButton {
     display:inline-block;
-    margin: 10px;
+    all: unset;
+    cursor: pointer;
+    img {
+      margin-right: 15px;
+    }
+  }
+  .inlineButton:active {
+    background:#fff6f1;
   }
 </style>
